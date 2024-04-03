40fc1e542edf6efc7bbb50780316b98475f442a2
DeEskalator <jakoebchen.41@gmail.com>
2024-04-03 10:50:01 +0200

    Bugfix, Included Stars in CSV

--- export_stars/export_stars.py
+++ export_stars/export_stars.py
@@ -1,14 +1,9 @@
-#!env python
-
 import sys
 import csv
-
 from math import ceil
 from argparse import ArgumentParser
-
 from github import Github
 from urllib3 import Retry
-
 
 def starred_repos(user):
     starred = user.get_starred()
@@ -18,7 +13,6 @@ def starred_repos(user):
         for repo in starred.get_page(page_num):
             yield repo
 
-
 def config_retry(backoff_factor=1.0, total=8):
     """urllib3 will sleep for:
         {backoff factor} * (2 ** ({number of total retries} - 1))
@@ -27,13 +21,11 @@ def config_retry(backoff_factor=1.0, total=8):
     Retry.DEFAULT_BACKOFF_MAX = backoff_factor * 2 ** (total - 1)
     return Retry(total=total, backoff_factor=backoff_factor)
 
-
 def parse_args():
-    parser = ArgumentParser(description="export a GitHub user's starred repositorys to CSV")
+    parser = ArgumentParser(description="export a GitHub user's starred repositories to CSV")
     parser.add_argument("--user")
     parser.add_argument("--github-token")
     return parser.parse_args()
-
 
 def main():
     args = parse_args()
@@ -41,14 +33,14 @@ def main():
         print("Please set `--user` to a valid GitHub user name.", file=sys.stderr)
         exit(1)
 
-    gh = Github(args.token, retry=config_retry()) if args.token else Github(retry=config_retry())
+    gh = Github(args.github_token, retry=config_retry()) if args.github_token else Github(retry=config_retry())
+
     user = gh.get_user(args.user)
 
     writer = csv.writer(sys.stdout)
 
     for repo in starred_repos(user):
-        writer.writerow((repo.html_url, repo.description))
-
+        writer.writerow((repo.html_url, repo.stargazers_count, repo.description))
 
 if __name__ == "__main__":
     main()
